f9c89db2ed090547460f892b4804485d18d2ef76
Jon T Gretarsson <jon@relateiq.com>
2014-04-03 23:26:20 -0700

    Expose host machine host and port

--- start.sh
+++ start.sh
@@ -5,11 +5,15 @@ export JMX_PORT=7203
 
 echo "ZOOKEEPER_IP=$ZOOKEEPER_PORT_2181_TCP_ADDR"
 
+IP=$(cat /etc/hosts | head -n1 | awk '{print $1}')
+PORT=9092
+
 cat /kafka/config/server.properties.tmpl \
   | sed "s|{{ZOOKEEPER_IP}}|${ZOOKEEPER_PORT_2181_TCP_ADDR:-localhost}|g" \
   | sed "s|{{BROKER_ID}}|${BROKER_ID:-0}|g" \
   | sed "s|{{CHROOT}}|${CHROOT:-}|g" \
-  | sed "s|{{EXPOSED_HOSTNAME}}|${EXPOSED_HOSTNAME:-localhost}|g" \
+  | sed "s|{{EXPOSED_HOSTNAME}}|${EXPOSED_HOSTNAME:-$IP}|g" \
+  | sed "s|{{EXPOSED_PORT}}|${EXPOSED_PORT:-$PORT}|g" \
    > /kafka/config/server.properties
 
 echo "Starting kafka"
