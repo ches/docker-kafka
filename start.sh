0d6b2a74fb7e8d5162f3af58574fcfae8c925cfa
Ches Martin <ches@whiskeyandgrits.net>
2015-08-24 23:02:10 +0700

    Only invoke sed once

--- start.sh
+++ start.sh
@@ -7,14 +7,14 @@
 
 IP=$(cat /etc/hosts | head -n1 | awk '{print $1}')
 
-cat /kafka/config/server.properties.template \
-  | sed "s|{{ZOOKEEPER_IP}}|${ZOOKEEPER_IP}|g" \
-  | sed "s|{{ZOOKEEPER_PORT}}|${ZOOKEEPER_PORT:-2181}|g" \
-  | sed "s|{{ZOOKEEPER_CHROOT}}|${ZOOKEEPER_CHROOT:-}|g" \
-  | sed "s|{{KAFKA_BROKER_ID}}|${KAFKA_BROKER_ID:-0}|g" \
-  | sed "s|{{KAFKA_ADVERTISED_HOST_NAME}}|${KAFKA_ADVERTISED_HOST_NAME:-$IP}|g" \
-  | sed "s|{{KAFKA_PORT}}|${KAFKA_PORT:-9092}|g" \
-  | sed "s|{{KAFKA_ADVERTISED_PORT}}|${KAFKA_ADVERTISED_PORT:-9092}|g" \
+cat /kafka/config/server.properties.template | sed \
+  -e "s|{{ZOOKEEPER_IP}}|${ZOOKEEPER_IP}|g" \
+  -e "s|{{ZOOKEEPER_PORT}}|${ZOOKEEPER_PORT:-2181}|g" \
+  -e "s|{{ZOOKEEPER_CHROOT}}|${ZOOKEEPER_CHROOT:-}|g" \
+  -e "s|{{KAFKA_BROKER_ID}}|${KAFKA_BROKER_ID:-0}|g" \
+  -e "s|{{KAFKA_ADVERTISED_HOST_NAME}}|${KAFKA_ADVERTISED_HOST_NAME:-$IP}|g" \
+  -e "s|{{KAFKA_PORT}}|${KAFKA_PORT:-9092}|g" \
+  -e "s|{{KAFKA_ADVERTISED_PORT}}|${KAFKA_ADVERTISED_PORT:-9092}|g" \
    > /kafka/config/server.properties
 
 # Kafka's built-in start scripts set the first three system properties here, but
