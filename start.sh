aa21f52568aeb0d75776a1187081dce4895690e0
Sean Kruzel <skruzel@astrocyte.io>
2016-07-13 12:09:12 -0400

    removed max.poll.records as it is a consumer setting and not a server setting

--- start.sh
+++ start.sh
@@ -22,7 +22,6 @@ cat /kafka/config/server.properties.template | sed \
   -e "s|{{ZOOKEEPER_CONNECTION_TIMEOUT_MS}}|${ZOOKEEPER_CONNECTION_TIMEOUT_MS:-10000}|g" \
   -e "s|{{ZOOKEEPER_SESSION_TIMEOUT_MS}}|${ZOOKEEPER_SESSION_TIMEOUT_MS:-10000}|g" \
   -e "s|{{GROUP_MAX_SESSION_TIMEOUT_MS}}|${GROUP_MAX_SESSION_TIMEOUT_MS:-300000}|g" \
-  -e "s|{{MAX_POLL_RECORDS}}|${MAX_POLL_RECORDS:-2147483647}|g" \
    > /kafka/config/server.properties
 
 # Kafka's built-in start scripts set the first three system properties here, but
